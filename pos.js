be8e02f2b273fa6bb5c1a840ebdb04bbfc6e5493
reggynoble <36063561+reggynoble@users.noreply.github.com>
2018-05-01 07:53:05 -0400

    Add files via upload

---
+++ pos.js
@@ -0,0 +1,7 @@
+function output(){
+    var value1 = parseInt(document.getElementById('value1').value);
+    var value2 = parseInt(document.getElementById('value2').value);
+    document.getElementById('result').innerHTML = value1 + value2;
+}
+
+</script>
